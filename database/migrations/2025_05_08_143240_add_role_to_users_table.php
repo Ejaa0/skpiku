814c3f8744e6997ccb5d9bc245833f58e892efc3
Ejaa0 <2282004@unai.edu>
2025-05-08 21:40:48 +0700

    role sudah di tambahkan di tabel users

---
+++ database/migrations/2025_05_08_143240_add_role_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Tambahkan kolom 'role' ke tabel users.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user')->after('email');
+        });
+    }
+
+    /**
+     * Kembalikan perubahan (hapus kolom 'role').
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
